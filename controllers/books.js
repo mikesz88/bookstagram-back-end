e2ce8c21bc9d12e52ad23a12472641216efc180f
michael <michaelsanchez8815@gmail.com>
2023-01-18 14:03:06 -0600

    test

--- controllers/books.js
+++ controllers/books.js
@@ -17,7 +17,9 @@ exports.getBook = asyncHandler(async (req, res, next) => {
   const book = await Book.findById(req.params.id);
 
   if (!book) {
-    return next(new ErrorResponse(`Book not found with id of ${req.params.id}`, 404));
+    return next(
+      new ErrorResponse(`Book not found with id of ${req.params.id}`, 404)
+    );
   }
 
   res.status(200).json({ success: true, data: book });
@@ -26,34 +28,34 @@ exports.getBook = asyncHandler(async (req, res, next) => {
 // @desc Create new s3Url Link
 // @route GET /api/v1/books/get/s3url
 // @access PRIVATE
-exports.createS3Url = asyncHandler( async (req, res, next) => {
-  const [ imageName, url ] = await generateUploadURL();
+exports.createS3Url = asyncHandler(async (req, res, next) => {
+  const [imageName, url] = await generateUploadURL();
 
   if (!url) {
-    return next(new ErrorResponse(`s3 Access Denied`, 500))
+    return next(new ErrorResponse(`s3 Access Denied`, 500));
   }
-
+  console.log({ success: true, key: imageName, photoUrl: url });
   res.status(200).json({ success: true, key: imageName, photoUrl: url });
-})
+});
 
 // @desc Delete s3 Image
 // @route GET /api/v1/books/deletes3image
 // @access PRIVATE
-exports.deleteS3Image = asyncHandler( async (req, res, next) => {
+exports.deleteS3Image = asyncHandler(async (req, res, next) => {
   const response = await deleteObject(req.params.key);
   console.log(response, 'response');
 
   if (!response) {
-    return next(new ErrorResponse(`s3 Access Denied`, 500))
+    return next(new ErrorResponse(`s3 Access Denied`, 500));
   }
 
   res.status(200).json({ success: true, response });
-})
+});
 
 // @desc Create new book
 // @route POST /api/v1/books
 // @access PRIVATE
-exports.createBook = asyncHandler( async (req, res, next) => {
+exports.createBook = asyncHandler(async (req, res, next) => {
   req.body.user = req.user.id;
   const book = await Book.create(req.body);
   res.status(201).json({ success: true, data: book });
@@ -70,15 +72,20 @@ exports.updateBook = asyncHandler(async (req, res, next) => {
   }
 
   if (book.user.toString() !== req.user.id && req.user.role !== 'admin') {
-    return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this book`, 401))
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this book`,
+        401
+      )
+    );
   }
 
   book = await Book.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
-  })
+  });
 
-  res.status(200).json({ success: true, data: book});
+  res.status(200).json({ success: true, data: book });
 });
 
 // @desc Delete book
@@ -86,16 +93,21 @@ exports.updateBook = asyncHandler(async (req, res, next) => {
 // @access PRIVATE
 exports.deleteBook = asyncHandler(async (req, res, next) => {
   const book = await Book.findById(req.params.id);
-  
+
   if (!book) {
-    res.status(400).json({ success: false })
+    res.status(400).json({ success: false });
   }
 
   if (book.user.toString() !== req.user.id && req.user.role !== 'admin') {
-    return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this book`, 401))
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to delete this book`,
+        401
+      )
+    );
   }
 
   book.remove();
-  
-  res.status(200).json({ success: true, data: {}});
+
+  res.status(200).json({ success: true, data: {} });
 });
