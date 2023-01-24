484d1978ec52f81d822608eddc564363cbbb5e8a
michael <michaelsanchez8815@gmail.com>
2023-01-24 15:12:06 -0600

    update package.json

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/error');
-const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const hpp = require('hpp');
@@ -37,7 +36,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
 app.use(hpp());
